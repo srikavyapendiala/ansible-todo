a6953815cae241cf5189591941788638aca47ed3
srikavyapendiala <82608952+srikavyapendiala@users.noreply.github.com>
2021-06-30 17:26:04 +0530

    Update create.sh

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -15,7 +15,7 @@ Instance_Create() {
   INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
   STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Name | xargs)
   if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated"  ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Project,Value=TODOSHOP}]" | jq
+    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Project,Value=TODO}]" | jq
   else
     echo "Instance ${COMPONENT} already exists"
   fi
@@ -24,13 +24,13 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0150601334RJI73YPN4U --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z0150601334RJI73YPN4U  --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inv
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
 
 if [ "$1" == "all" ]; then
-  for instance in frontend login users todo redis; do
+  for instance in frontend redis users login todo ; do
     Instance_Create $instance-dev
   done
 else
